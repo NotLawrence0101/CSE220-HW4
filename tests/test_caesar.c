9bcac5cf43e12ca621e117437efa0c7d5d2ab07b
NotLawrence0101 <94724659+NotLawrence0101@users.noreply.github.com>
2026-03-13 08:38:53 +0000

    test cases

--- tests/test_caesar.c
+++ tests/test_caesar.c
@@ -4,16 +4,53 @@
 #include <criterion/criterion.h>
 #include "../cse_caesar.h"
 
-Test(encryptCaesar, basic) {
+// encryptCaesar test cases
+Test(encryptCaesar, encryptCaesar_Basic_Test) {
     char buf[32] = "xxxxxxxxxxxxxxxxxxxxxxxx";
-    int r = encryptCaesar("abc", buf, 2);
-    cr_expect_eq(r, 3);
-    cr_expect_str_eq(buf, "ceg__EOM__");
+    int r = encryptCaesar("hello", buf, 2);
+    cr_expect_eq(r, 5);
+    cr_expect_str_eq(buf, "jhpqu__EOM__");
 }
 
-Test(decryptCaesar, basic) {
-    char buf[32] = "xxxxxxxx";
-    int r = decryptCaesar("ceg__EOM__", buf, 2);
-    cr_expect_eq(r, 3);
-    cr_expect_str_eq(buf, "abc");
+Test(encryptCaesar, encryptCaesar_NULL_Test) {
+    char buf[32] = "xxxxxxxxxxxxxxxxxxxxxxxx";
+    int r = encryptCaesar(NULL, buf, 2);
+    cr_expect_eq(r, -2);
+    cr_expect_str_eq(buf, "xxxxxxxxxxxxxxxxxxxxxxxx");
+}
+
+Test(encryptCaesar, encryptCaesar_TooSmall_Test) {
+    char buf[8] = "xxxxxxxx";
+    int r = encryptCaesar("hello", buf, 8);
+    cr_expect_eq(r, -1);
+    cr_expect_str_eq(buf, "xxxxxxxx");
+}
+
+Test(encryptCaesar, encryptCaesar_TooSmall_Test) {
+    char buf[8] = "xxxxxxxx";
+    int r = encryptCaesar("hello", buf, 8);
+    cr_expect_eq(r, -1);
+    cr_expect_str_eq(buf, "xxxxxxxx");
+}
+
+// decryptCaesar test cases
+Test(decryptCaesar, decryptCaesar_Basic_Test) {
+    char buf[32] = "xxxxxxxxxxxxxxxxxxxxxxxx";
+    int r = decryptCaesar("jhpqu__EOM__", buf, 2);
+    cr_expect_eq(r, 5);
+    cr_expect_str_eq(buf, "hello");
+}
+
+Test(decryptCaesar, decryptCaesar_NULL_Test) {
+    char buf[32] = "xxxxxxxxxxxxxxxxxxxxxxxx";
+    int r = decryptCaesar(NULL, buf, 2);
+    cr_expect_eq(r, -2);
+    cr_expect_str_eq(buf, "xxxxxxxxxxxxxxxxxxxxxxxx");
+}
+
+Test(decryptCaesar, decryptCaesar_NoEOMMarker_Test) {
+    char buf[8] = "xxxxxxxx";
+    int r = decryptCaesar("jhpqu", buf, 2);
+    cr_expect_eq(r, -1);
+    cr_expect_str_eq(buf, "xxxxxxxx");
 }
--- tests/test_strg.c
+++ tests/test_strg.c
@@ -4,165 +4,327 @@
 #include <criterion/criterion.h>
 #include "../strgPtr.h"
 
-Test(strgLen, lengthTests){
+// strgLen test cases
+Test(strgLen, strgLen_Hello_Test){
     cr_expect_eq(strgLen("Hello"), 5);
+}
+
+Test(strgLen, strgLen_2Word_Test){
     cr_expect_eq(strgLen("Hello World"), 11);
+}
+Test(strgLen, strgLen_Symbol_Test){
     cr_expect_eq(strgLen("World!"), 6);
+}
+
+Test(strgLen, strgLen_EmptyString_Test){
     cr_expect_eq(strgLen(""), 0);
+}
+
+Test(strgLen, strgLen_NULL_Test){
     cr_expect_eq(strgLen(NULL), -1);
+}
+
+Test(strgLen, strgLen_MultipleSpaces_Test){
     cr_expect_eq(strgLen("   "), 3);
+}
+
+Test(strgLen, strgLen_OneChar_Test){
     cr_expect_eq(strgLen("a"), 1);
+}
+
+Test(strgLen, strgLen_Numbers_Test){
     cr_expect_eq(strgLen("12345"), 5);
+}
+
+Test(strgLen, strgLen_LettersNumbers_Test){
     cr_expect_eq(strgLen("abcde12345"), 10);
+}
+
+Test(strgLen, strgLen_Symbols_Test){
     cr_expect_eq(strgLen("!@#$%^&*()"), 10);
 }
 
-Test(strgCopy, copyTests){
-    char a[5];
-    strgCopy("123456789", a, 5);
-    cr_expect_str_eq(a, "1234");
+// strgCopy test cases
+Test(strgCopy, strgCopy_Numbers_Test){
+    char d[5];
+    strgCopy("123456789", d, 5);
+    cr_expect_str_eq(d, "1234");
+}
 
-    char b[2];
-    strgCopy("Hello", b, 2);
-    cr_expect_str_eq(b, "H");
+Test(strgCopy, strgCopy_Char_Test){
+    char d[2];
+    strgCopy("Hello", d, 2);
+    cr_expect_str_eq(d, "H");
+}
 
-    char c[1];
-    strgCopy("Hello", c, 1);
-    cr_expect_str_eq(c, "");
+Test(strgCopy, strgCopy_Terminator_Test){
+    char d[1];
+    strgCopy("Hello", d, 1);
+    cr_expect_str_eq(d, "");
+}
 
+Test(strgCopy, strgCopy_2Words_Test){
     char d[17];
     strgCopy("Computer Science", d, 17);
     cr_expect_str_eq(d, "Computer Science");
-
-    char e[25];
-    strgCopy("Long Array", e, 25);
-    cr_expect_str_eq(e, "Long Array");
-    
-    char f[0];
-    strgCopy("Hello", f, 0);
-    cr_expect_str_eq(f, "");
-
-    char g[10];
-    strgCopy(NULL, g, 10); 
-    cr_expect_str_eq(g, "");
-
-    char h[23];
-    strgCopy("!@#$%^&*()", h, 23);
-    cr_expect_str_eq(h, "!@#$%^&*()");
-
-    char i[10];
-    strgCopy("     ", i, 10);
-    cr_expect_str_eq(i, "     ");
-
-    strgCopy("      a", i, 10);
-    cr_expect_str_eq(i, "      a");
 }
 
-Test(strgChangeCase, changeCasesTest){
+Test(strgCopy, strgCopy_LargerArray_Test){
+    char d[25];
+    strgCopy("Long Array", d, 25);
+    cr_expect_str_eq(d, "Long Array");
+}
+
+Test(strgCopy, strgCopy_EmptyArray_Test){
+    char d[0];
+    strgCopy("Hello", d, 0);
+    cr_expect_str_eq(d, "");
+}
+
+Test(strgCopy, strgCopy_NULLString_Test){
+    char d[10] = "";
+    strgCopy(NULL, d, 10); 
+    cr_expect_str_eq(d, "");
+}
+
+Test(strgCopy, strgCopy_Symbols_Test){
+    char d[23];
+    strgCopy("!@#$%^&*()", d, 23);
+    cr_expect_str_eq(d, "!@#$%^&*()");
+}
+
+Test(strgCopy, strgCopy_Spaces_Test){
+    char d[10];
+    strgCopy("     ", d, 10);
+    cr_expect_str_eq(d, "     ");
+}
+
+Test(strgCopy, strgCopy_SpacesChar_Test){
+    char d[10];
+    strgCopy("      a", d, 10);
+    cr_expect_str_eq(d, "      a");
+}
+
+// strgChangeCase test cases
+Test(strgChangeCase, strgChangeCase_Hello_Test){
     char s[] = "Hello";
     strgChangeCase(s);
     cr_expect_str_eq(s, "hELLO");
-
-    char s1[] = "h1llo";
-    strgChangeCase(s1);
-    cr_expect_str_eq(s1, "h1lLO");
-
-    char s2[] = "h1";
-    strgChangeCase(s2);
-    cr_expect_str_eq(s2, "h1");
-
-    char s3[] = "1234567";
-    strgChangeCase(s3);
-    cr_expect_str_eq(s3, "1234567");
-    
-    char s4[] = "h";
-    strgChangeCase(s4);
-    cr_expect_str_eq(s4, "H");
-
-    char s5[] = "";
-    strgChangeCase(s5);
-    cr_expect_str_eq(s5, "");
-
-    char s6[] = "1";
-    strgChangeCase(s6);
-    cr_expect_str_eq(s6, "1");
-    
-    char s7[] = "C4ps 1s N0t On";
-    strgChangeCase(s7);
-    cr_expect_str_eq(s7, "C4pS 1s N0t oN");
-
-    char s8[] = "1h";
-    strgChangeCase(s8);
-    cr_expect_str_eq(s8, "1h");
-
-    char s9[] = "   ";
-    strgChangeCase(s9);
-    cr_expect_str_eq(s9, "   ");
 }
 
-Test(strgDiff, diffTest){
+Test(strgChangeCase, strgChangeCase_Adjacency_Test){
+    char s[] = "h1llo";
+    strgChangeCase(s);
+    cr_expect_str_eq(s, "h1lLO");
+}
+
+Test(strgChangeCase, strgChangeCase_2CharAdjacency_Test){
+    char s[] = "h1";
+    strgChangeCase(s);
+    cr_expect_str_eq(s, "h1");
+}
+
+Test(strgChangeCase, strgChangeCase_Numbers_Test){
+    char s[] = "1234567";
+    strgChangeCase(s);
+    cr_expect_str_eq(s, "1234567");
+}
+
+Test(strgChangeCase, strgChangeCase_Char_Test){
+    char s[] = "h";
+    strgChangeCase(s);
+    cr_expect_str_eq(s, "H");
+}
+
+Test(strgChangeCase, strgChangeCase_EmptyString_Test){
+    char s[] = "";
+    strgChangeCase(s);
+    cr_expect_str_eq(s, "");
+}
+ 
+Test(strgChangeCase, strgChangeCase_LongerString_Test){
+    char s[] = "C4ps 1s N0t On";
+    strgChangeCase(s);
+    cr_expect_str_eq(s, "C4pS 1s N0t oN");
+}
+
+Test(strgChangeCase, strgChangeCase_NumberFirst_Test){
+    char s[] = "1h";
+    strgChangeCase(s);
+    cr_expect_str_eq(s, "1h");
+}
+
+Test(strgChangeCase, strgChangeCase_Spaces_Test){
+    char s[] = "   ";
+    strgChangeCase(s);
+    cr_expect_str_eq(s, "   ");
+}
+
+Test(strgChangeCase, strgChangeCase_NULL_Test){
+    char *s = NULL;
+    strgChangeCase(s);
+    cr_expect_eq(s, NULL);
+}
+
+// strgDiff test cases
+Test(strgDiff, strgDiff_SameString_Test){
     cr_expect_eq(strgDiff("Hello", "Hello"), -1);
+}
+
+Test(strgDiff, strgDiff_DiffString_Test){
     cr_expect_eq(strgDiff("H3llo", "Hello"), 1);
+}
+
+Test(strgDiff, strgDiff_BothEmpty_Test){
     cr_expect_eq(strgDiff("", ""), -1);
+}
+
+Test(strgDiff, strgDiff_FirstCharDiff_Test){
     cr_expect_eq(strgDiff("a", ""), 0);
+}
+
+Test(strgDiff, strgDiff_SecondCharDiff_Test){
     cr_expect_eq(strgDiff("", "a"), 0);
+}
+
+Test(strgDiff, strgDiff_FirstStringNull_Test){
     cr_expect_eq(strgDiff(NULL, "NULL"), -2);
+}
+
+Test(strgDiff, strgDiff_BothStringNull_Test){
     cr_expect_eq(strgDiff(NULL, NULL), -2);
+}
+
+Test(strgDiff, strgDiff_SecondStringNull_Test){
     cr_expect_eq(strgDiff("NULL", NULL), -2);
+}
+
+Test(strgDiff, strgDiff_LengthDifference_Test){
     cr_expect_eq(strgDiff("Hello World!", "Hello Worl"), 10);
+}
+
+Test(strgDiff, strgDiff_Spaces_Test){
     cr_expect_eq(strgDiff("     ", "      "), 5);
 }
 
-Test(strgInterleave, studentTest){
-    char a[6];
-    strgInterleave("abcdefghi", "123456789", a, 6);
-    cr_expect_str_eq(a, "a1b2c");
+// strgInterleave test cases
+Test(strgInterleave, strgInterleave_Basic_Test){
+    char d[6];
+    strgInterleave("abcdefghi", "123456789", d, 6);
+    cr_expect_str_eq(d, "a1b2c");
+}
 
-    char b[20];
-    strgInterleave("abcdefghi", "123", b, 20);
-    cr_expect_str_eq(b, "a1b2c3defghi");
+Test(strgInterleave, strgInterleave_DifferentLengths1_Test){
+    char d[20];
+    strgInterleave("abcdefghi", "123", d, 20);
+    cr_expect_str_eq(d, "a1b2c3defghi");
+}
 
-    char c[20];
-    strgInterleave("abc", "123456789", c, 20);
-    cr_expect_str_eq(c, "a1b2c3456789");
+Test(strgInterleave, strgInterleave_DifferentLengths2_Test){
+    char d[20];
+    strgInterleave("abc", "123456789", d, 20);
+    cr_expect_str_eq(d, "a1b2c3456789");
+}
 
+Test(strgInterleave, strgInterleave_Char_Test){
     char d[2];
     strgInterleave("abcd", "12345", d, 2);
     cr_expect_str_eq(d, "a");
-
-    char e[20];
-    strgInterleave(NULL, "12345", e, 20);
-    cr_expect_str_eq(e, "");
-
-    strgInterleave("abcd", NULL, e, 20);
-    cr_expect_str_eq(e, "");
-
-    strgInterleave(NULL, NULL, e, 20);
-    cr_expect_str_eq(e, "");
-
-    char f[10];
-    strgInterleave("abcde", "    ", f, 10);
-    cr_expect_str_eq(f, "a b c d e");
-
-    char g[20];
-    strgInterleave("!@#$%", "     ", g, 20);
-    cr_expect_str_eq(g, "! @ # $ % ");
-
-    char h[20];
-    strgInterleave("", "", h, 20);
-    cr_expect_str_eq(h, "");
 }
 
-Test(strgReverseLetters, studentTest){
-    char a[] = "Test";
-    strgReverseLetters(a);
-    cr_expect_str_eq(a, "tseT");
+Test(strgInterleave, strgInterleave_FirstNULL_Test){
+    char d[20];
+    strgInterleave(NULL, "12345", d, 20);
+    cr_expect_str_eq(d, "");
+}
 
-    char b[] = "W0ah";
-    strgReverseLetters(b);
-    cr_expect_str_eq(b, "h0aW");
+Test(strgInterleave, strgInterleave_SecondNULL_Test){
+    char d[10];
+    strgInterleave("abcd", NULL, d, 10);
+    cr_expect_str_eq(d, "");
+}
 
-    char c[] = "";
-    strgReverseLetters(c);
-    cr_expect_str_eq(c, "");
+Test(strgInterleave, strgInterleave_BothNULL_Test){
+    char d[5];
+    strgInterleave(NULL, NULL, d, 5);
+    cr_expect_str_eq(d, "");
+}
+
+Test(strgInterleave, strgInterleave_Spaces_Test){
+    char d[10];
+    strgInterleave("abcde", "    ", d, 10);
+    cr_expect_str_eq(d, "a b c d e");
+}
+
+Test(strgInterleave, strgInterleave_Symbols_Test){
+    char d[20];
+    strgInterleave("!@#$%", "     ", d, 20);
+    cr_expect_str_eq(d, "! @ # $ % ");
+}
+
+Test(strgInterleave, strgInterleave_EmptyStrings_Test){
+    char d[20];
+    strgInterleave("", "", d, 20);
+    cr_expect_str_eq(d, "");
+}
+
+// strgReverseLetters test cases
+Test(strgReverseLetters, strgReverseLetters_Lowercase_Test){
+    char d[] = "test";
+    strgReverseLetters(d);
+    cr_expect_str_eq(d, "tset");
+}
+
+Test(strgReverseLetters, strgReverseLetters_Uppercase_Test){
+    char d[] = "TEST";
+    strgReverseLetters(d);
+    cr_expect_str_eq(d, "TSET");
+}
+
+Test(strgReverseLetters, strgReverseLetters_BothCases_Test){
+    char d[] = "TeSt";
+    strgReverseLetters(d);
+    cr_expect_str_eq(d, "tSeT");
+}
+
+Test(strgReverseLetters, strgReverseLetters_Number_Test){
+    char d[] = "T3st";
+    strgReverseLetters(d);
+    cr_expect_str_eq(d, "t3sT");
+}
+
+Test(strgReverseLetters, strgReverseLetters_Empty_Test){
+    char d[] = "";
+    strgReverseLetters(d);
+    cr_expect_str_eq(d, "");
+}
+
+Test(strgReverseLetters, strgReverseLetters_NULL_Test){
+    char *d = NULL;
+    strgReverseLetters(d);
+    cr_expect_eq(d, NULL);
+}
+
+Test(strgReverseLetters, strgReverseLetters_NotLetters_Test){
+    char d[] = "*(07734)*";
+    strgReverseLetters(d);
+    cr_expect_str_eq(d, "*(07734)*");
+}
+
+Test(strgReverseLetters, strgReverseLetters_Palindrome_Test){
+    char d[] = "racecar";
+    strgReverseLetters(d);
+    cr_expect_str_eq(d, "racecar");
+}
+
+Test(strgReverseLetters, strgReverseLetters_TwoWords_Test){
+    char d[] = "Hello World!";
+    strgReverseLetters(d);
+    cr_expect_str_eq(d, "dlroW olleH!");
+}
+
+Test(strgReverseLetters, strgReverseLetters_SymbolsBetween_Test){
+    char d[] = "a#!)b($())c#*d";
+    strgReverseLetters(d);
+    cr_expect_str_eq(d, "d#!)c($())b#*a");
 }
